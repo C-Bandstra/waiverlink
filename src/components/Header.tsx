8b964442152c152ab95708af528825debe7ad16e
C-Bandstra <bandstracharlie@gmail.com>
2025-05-23 02:52:28 -0600

    Create basic brand Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full px-4 pb-4 border-b border-gray-200 bg-white">
+      <div className="mx-auto flex items-center">
+        <h1 className="text-xl font-bold text-gray-900">waiverlink</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
