cf42902763d6a16ae89da00be721f96031ec7a67
C-Bandstra <bandstracharlie@gmail.com>
2025-05-27 17:21:39 -0600

    Admin navigation setup; navigation blueprint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import {useNavigate, useLocation} from "react-router-dom";
+import { useSeed } from "../context/SeedContext";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const seed = useSeed();
+
+  const isOnAdminPage = location.pathname.includes(`/${seed.id}/admin`);
+  
   return (
-    <header className="w-full px-4 pb-4 border-b border-gray-200 bg-white">
-      <div className="mx-auto flex items-center">
+    <header className="w-full p-2 border-b border-gray-200 bg-white flex justify-between">
+      <div className="flex items-center">
         <h1 className="text-xl font-bold text-gray-900">waiverlink</h1>
       </div>
+      <button
+        onClick={() => navigate(`/${seed.id}/admin`)}
+        className="border-[2px] border-black rounded-sm text-black font-bold p-2 transition-transform duration-300 transform hover:scale-110 will-change-transform"
+      >
+        {isOnAdminPage ? "manage" : "admin"}
+      </button>
     </header>
   );
 };
