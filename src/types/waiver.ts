b6e7ccca8d6f55633ac63c330b82c10f094b89cd
C-Bandstra <bandstracharlie@gmail.com>
2025-05-27 17:44:17 -0600

    Define SubType + Create WaiverScreenProps (notification system on hold)

--- src/types/waiver.ts
+++ src/types/waiver.ts
@@ -8,10 +8,21 @@ export type Waiver = {
   signedAt: string
 }
 
+export interface WaiverScreenProps {
+  // setNotifications: React.Dispatch<React.SetStateAction<string>>;
+}
+
 export interface WaiverToken {
   type: string; // e.g., "name", "signature", "date", "input"
   id: number;  // count of this type in the waiver string (1-based)
   subtype?: string | null;  // optional subtype or identifier (e.g., "boardModel")
+}
+
+export interface SubType {
+  //Currently not used until issues ironed out; still good for reference
+  fieldName: string | null , //fieldName extracted directly after first colon
+  options: Array<string> | [] , //options extracted after each colon after the fieldName (:) 
+  label: string | null //label extracted after semi-colon(;) (input:fieldName:option1;This is a label)
 }
 
 export interface FieldDefinition {
