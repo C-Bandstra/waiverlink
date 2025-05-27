cdb294cde49b7c529d01725fa7ee6144ce809625
C-Bandstra <bandstracharlie@gmail.com>
2025-05-27 17:30:17 -0600

    Create PageContainer for page-level updates/notifications

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// PageContainer just wraps the routed-to page; might be useful down the line for page updates/notifcations
+const PageContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+
+  return (
+    <div className="p-4">
+      {children}
+    </div>
+  );
+};
+
+export default PageContainer;
