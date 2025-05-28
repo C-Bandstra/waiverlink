40f62b4cb3546ccfbf0a8bae1c75ad97f9bb0932
C-Bandstra <bandstracharlie@gmail.com>
2025-05-27 19:36:41 -0600

    Move Admin.tsx to own pages directory

---
+++ src/pages/admin/Admin.tsx
@@ -0,0 +1,13 @@
+// pages/admin/AdminDashboard.tsx
+import React from "react";
+import Dashboard from "./Dashboard";
+
+const Admin: React.FC = () => {
+  return (
+    <div className="max-w-5xl mt-8">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default Admin;
