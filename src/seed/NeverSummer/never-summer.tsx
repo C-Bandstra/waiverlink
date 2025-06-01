c120bd47708cc3b9fcd824f5278a03719174af74
C-Bandstra <bandstracharlie@gmail.com>
2025-05-31 19:18:08 -0600

    Use correct typing for waiverTemplates array

--- src/seed/NeverSummer/never-summer.tsx
+++ src/seed/NeverSummer/never-summer.tsx
@@ -3,15 +3,16 @@ import type { FieldDefinition } from '../../types/waiver';
 import { demoWaiverTemplate, adminWaiverTemplate } from './templates';
 import logo from "../../assets/never-summer.png"
 import { baseFieldDefinitions } from '../../fields/fieldDefinitions/main';
+import type { Template } from '../../types/admin';
 
 const NeverSummer = {
-  id: "never_summer",
+  id: "never-summer",
   name: "Never Summer",
   image: logo,
   waiverTemplates: {
     demo: demoWaiverTemplate,
-    admin: adminWaiverTemplate,
-  },
+    admin: adminWaiverTemplate, //copy of demo to assume list of templates
+  } as Record<string, Template>,
   trackingLabels: ["boardModel"],
   fieldDefinitions: baseFieldDefinitions satisfies Record<string, FieldDefinition>,
 };
