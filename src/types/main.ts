ae8bdeef8cd1920979254ba881ca741a44b822ec
C-Bandstra <bandstracharlie@gmail.com>
2025-05-31 19:20:00 -0600

    Add AppRoute for routing children and base components

--- src/types/main.ts
+++ src/types/main.ts
@@ -1,5 +1,7 @@
-export type MainProps = {
-  // params: {
-  //   seedId: string;
-  // };
-};
+import type { ComponentType } from "react";
+
+export interface AppRoute {
+  path: string;               // always a string, empty string for "index"
+  component: ComponentType<any>;
+  children?: AppRoute[];
+}
