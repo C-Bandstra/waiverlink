52f9a1e974ea24ad2eb69169b7965e3e8806990c
Charlie Bandstra <bandstracharlie@gmail.com>
2025-06-09 23:57:23 -0500

    Build base Christy Sports template

---
+++ src/seed/ChristySports/templates.ts
@@ -0,0 +1,15 @@
+export const gearRentalWaiverTemplate = {
+  id: "gear-rental",
+  title: "Gear Rental Waiver",
+  groupingId: "waivers",
+  content: `
+    I, {{name}}, acknowledge and agree to the terms and conditions outlined in this release of liability. I understand that snowboarding, demoing equipment, and related activities involve inherent risks. I voluntarily assume full responsibility for any injuries or damages that may occur during my participation in a Christy Sports event or while using Christy Sports gear.
+    {{br}}
+    Participant Signature: {{signature}}
+    {{br}}
+    Date: {{date:current|color:blue;}}
+    {{br}}
+    Participant Signature: {{signature|font-size:40px;color:red}}
+    {{br}}
+    `
+  }
