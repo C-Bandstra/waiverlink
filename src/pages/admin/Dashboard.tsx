a337388f96ac139f2f08477a2c8ab5310e43bf6e
Charlie Bandstra <bandstracharlie@gmail.com>
2025-06-11 17:52:58 -0500

    Add whitespace

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -5,6 +5,7 @@ import { useOutletContext } from "react-router-dom";
 
 const Dashboard: React.FC = () => {
   const { waiverSubmissions } = useOutletContext<DashboardContext>();
+
   return (
     <div className="p-4 border-2 border-black">
       Dashboard
