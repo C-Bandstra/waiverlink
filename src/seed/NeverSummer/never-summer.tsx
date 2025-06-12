efe38a7d399d5f58c84676e9b4a3e77fc9caa40f
Charlie Bandstra <bandstracharlie@gmail.com>
2025-06-12 02:55:48 -0500

    Add multi-signer template for testing

--- src/seed/NeverSummer/never-summer.tsx
+++ src/seed/NeverSummer/never-summer.tsx
@@ -1,5 +1,5 @@
 import type { FieldDefinition } from '../../types/waiver';
-import { demoWaiverTemplate, adminWaiverTemplate } from './templates';
+import { demoWaiverTemplate, adminWaiverTemplate, multiSignerTemplate } from './templates';
 import logo from "../../assets/never-summer.png"
 import { baseFieldDefinitions } from '../../fields/fieldDefinitions/main';
 import type { Template } from '../../types/admin';
@@ -11,6 +11,7 @@ const NeverSummer = {
   waiverTemplates: {
     demo: demoWaiverTemplate,
     admin: adminWaiverTemplate, //copy of demo to assume list of templates
+    "multi-signer": multiSignerTemplate,
   } as Record<string, Template>,
   trackingLabels: ["boardModel"],
   fieldDefinitions: baseFieldDefinitions satisfies Record<string, FieldDefinition>,
