8fe3648acbd5738cfec34a2a4f4b8d1e545b1894
Charlie Bandstra <bandstracharlie@gmail.com>
2025-06-16 19:27:09 -0500

    Save collection with template slug layer

--- src/firebase/submission/submitWaiver.ts
+++ src/firebase/submission/submitWaiver.ts
@@ -1,23 +1,36 @@
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase";
 import type { WaiverSubmission } from "../../types/admin";
+import { toSlug } from "../../utils/helpers";
 
+/**
+ * Submits a waiver under the following Firestore structure:
+ * seeds/:seedId/:groupingId/:templateTitle/:docId
+ */
 export async function submitWaiver(
   seedId: string,
   groupingId: string,
+  templateTitle: string,
   submission: WaiverSubmission
 ): Promise<string> {
   try {
+    const templateSlug = toSlug(templateTitle); // => "multi-signer-waiver"
+
     const submissionToSave = {
       ...submission,
       serverTimestamp: serverTimestamp(),
     };
 
-    const submissionsRef = collection(db, "seeds", seedId, groupingId);
-    console.log(submissionsRef)
+    const submissionsRef = collection(
+      db,
+      "seeds",
+      seedId,
+      groupingId,
+      templateSlug,
+      "submissions"
+    );
 
     const docRef = await addDoc(submissionsRef, submissionToSave);
-
     return docRef.id;
   } catch (error) {
     console.error("Error submitting waiver:", error);
