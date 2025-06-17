899eae1f095143df2b51dc86839297ee10b9faa0
Charlie Bandstra <bandstracharlie@gmail.com>
2025-06-16 19:30:07 -0500

    Supply submission Outlet context with template slug based submissions

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -3,22 +3,33 @@ import { useEffect, useState } from "react";
 import { listenToSubmissions } from "../../firebase/admin/listenToSubmissions";
 import { useSeed } from "../../context/SeedContext";
 import type { WaiverSubmission } from "../../types/admin";
+import { toSlug } from "../../utils/helpers";
 
 const Admin: React.FC = () => {
   const [waiverSubmissions, setWaiverSubmissions] = useState<WaiverSubmission[]>([]);
+  const [selectedTemplateTitle, setSelectedTemplateTitle] = useState<string | null>(null);
   const seed = useSeed();
 
   useEffect(() => {
-    const unsub = listenToSubmissions(seed.id, "waivers", setWaiverSubmissions);
+    if (!selectedTemplateTitle) return;
+    const selectedTemplateSlug = toSlug(selectedTemplateTitle);
+
+    const unsub = listenToSubmissions(seed.id, "waivers", selectedTemplateSlug, setWaiverSubmissions);
     return () => unsub?.();
-  }, [seed.id]);
+  }, [seed.id, selectedTemplateTitle]);
 
   return (
     <>
       <p>Admin</p>
-      <Outlet context={{ waiverSubmissions }} />
+      <Outlet
+        context={{
+          waiverSubmissions,
+          setSelectedTemplateTitle,
+          selectedTemplateTitle,
+        }}
+      />
     </>
-);
+  );
 };
 
 export default Admin;
